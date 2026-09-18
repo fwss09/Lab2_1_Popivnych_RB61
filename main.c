4a64dbde956677a9662ead5407c96e3bd584c61d
Andrii Popivnych <andreipopivnich@gmail.com>
2026-09-18 21:39:58 +0300

    complete lab

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <math.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    double X, res;
+
+    printf("Enter x: ");
+    scanf("%lf", &X);
+
+    if(0 <= X && X < 4) {
+        res = (2*X-1)/(3*X+1);
+    } else if(4 <= X && X < 12) {
+        res = 5*X+10;
+    } else if( 15 <= X && X < 55) {
+        res = (2*X/sqrt(-X+2*pow(X, 2)))-3*pow(X, 2);
+    } else {
+        res = 2-(X/10);
+    }
+
+    printf("res: %f", res);
     return 0;
 }
